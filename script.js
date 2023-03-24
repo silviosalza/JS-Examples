fc4773d782712e6867dbda4836b4aa30df4f2023
silviosalza <silviofilipposalza@gmail.com>
2023-03-24 11:52:04 +0100

    push

--- script.js
+++ script.js
@@ -43,15 +43,38 @@ const namesArray = ["Silvio", "Hector", "John"]
 const [firstName, secondName, thirdName] = namesArray;
 console.log(firstName, thirdName);
 
-
 const student = {
     name: "Silvio",
     età: "33",
     email: "silvio@gmail.com",
 }
-
 //questa sugar syntax funziona solo se il nome delle variabili è UGUALE al nome delle chiavi dell'oggetto
 const {name, età, email} = student
-
 console.log(name, age, email);
+
+
+
+//! note su array
+const names = ["Silvio", "Hector", "John"]
+//voglio copiare array in un altra variabile
+const copyArray = names
+// questa operazione NON sto creando un nuovo array, ma creo un altra etichetta che punta sempre sulla stessa area di memoria
+copyArray.push("Italo")
+console.log(names);
+console.log(copyArray);
+//!spread operator [...] scompone array in pezzettini names e li mette dentro copyarray2
+const copyArray2 = ["Moka", ...names, "Bolina"]
+
+console.log(copyArray2);
+
+//! rest operator (solo come argomento di una funzione)
+
+const result = sumNumbers(2,5, 10, 23, 34)
+console.log(result);
+function sumNumbers(...numbers){
+    console.log(numbers);
+    let sum = 0
+    numbers.forEach((num) => {sum += num})
+    return sum;
+}
 
